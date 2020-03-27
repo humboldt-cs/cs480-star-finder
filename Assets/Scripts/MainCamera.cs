b2479ca66a4e8924b5c24124edd391203f15c9cc
j-feigner <jordan.feigner.r@gmail.com>
2020-03-26 18:09:08 -0700

    Adding completed Unity project files
    This should update the repository to our current state of progress.

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var orientation = new Vector3(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"), 0.0f);
+        transform.Rotate(orientation);
+    }
+}
