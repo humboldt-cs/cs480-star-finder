54d025818811025baac1e0a4675edf7e7da28003
j-feigner <jordan.feigner.r@gmail.com>
2020-03-30 13:01:47 -0700

    Added outline for coordinate conversion function
    Located in MainCamera script

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -7,7 +7,7 @@ public class MainCamera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -15,5 +15,21 @@ public class MainCamera : MonoBehaviour
     {
         var orientation = new Vector3(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"), 0.0f);
         transform.Rotate(orientation);
+    }
+
+    // Conversion from celestial RA and DEC values to a usable transform vector
+    vector3 CoordConversion(double right_ascension, double declination)
+    {
+        const double DISTANCE_MOD = 10;
+        vector3 transform;
+        double x, y, z;
+
+        x = Mathf.Sin(right_ascension);
+        y = Mathf.Cos(right_ascension);
+        z = Mathf.Sin(declination);
+
+        transform = new vector3(x, y, z) * DISTANCE_MOD;
+
+        return transform;
     }
 }
