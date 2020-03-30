b5a4592d4484bb373a4b812f8db5675100b81d7f
j-feigner <jordan.feigner.r@gmail.com>
2020-03-30 13:42:58 -0700

    Added Sirius test
    Prefab successfully loads at runtime.

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class MainCamera : MonoBehaviour
 {
+    // Placeholder for Sirius
+    public GameObject starPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        // Test star generation data
+        // Sirius - RA: 101.2875, Dec: -16.7161
+        Instantiate(starPrefab, CoordConversion(101.2875f, -16.7161f), Quaternion.identity);
     }
 
     // Update is called once per frame
@@ -18,11 +23,10 @@ public class MainCamera : MonoBehaviour
     }
 
     // Conversion from celestial RA and DEC values to a usable transform vector
+    // Expected RA value to be in degrees
     Vector3 CoordConversion(float right_ascension, float declination)
     {
-        //This value is only for making the scene more managable in Unity editor by moving objects farther from the camera
-        const float DISTANCE_MOD = 10.0f;
-
+        const float DISTANCE_MOD = 10.0f; // This value is only for making the scene more managable in Unity editor by moving objects farther from the camera
         Vector3 transform;
         float x, y, z;
 
