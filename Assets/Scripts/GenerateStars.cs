efc5603d68daaba568bf5e89203e8b66920bcfac
j-feigner <jordan.feigner.r@gmail.com>
2020-03-30 18:04:22 -0700

    Created function to convert raw RA value to radians

--- Assets/Scripts/GenerateStars.cs
+++ Assets/Scripts/GenerateStars.cs
@@ -10,6 +10,9 @@ public class GenerateStars : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Test call of RA->Deg
+        float temp = RightAscensionToDegrees(64508.9f);
+
         // Test star generation data
         // Sirius - RA: 101.2875, Dec: -16.7161
         Instantiate(starPrefab, CoordConversion(101.2875f, -16.7161f), Quaternion.identity);
@@ -18,11 +21,11 @@ public class GenerateStars : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Intentionally empty: stars will be fixed
     }
 
     // Conversion from celestial RA and DEC values to a usable transform vector
-    // Expected RA/Dec values to be in degrees
+    // Expected RA/Dec values to be in radians
     Vector3 CoordConversion(float right_ascension, float declination)
     {
         const float DISTANCE_MOD = 10.0f; // This value is only for making the scene more managable in Unity editor by moving objects farther from the camera
@@ -36,5 +39,29 @@ public class GenerateStars : MonoBehaviour
         transform = new Vector3(x, y, z);
 
         return transform;
+    }
+
+    float RightAscensionToDegrees(float right_ascension)
+    {
+        // Grab arcseconds from raw data
+        float ss = Mathf.Round(right_ascension % 100);
+
+        // Grab arcminutes from raw data
+        float mm = Mathf.Floor((right_ascension % 10000) / 100);
+
+        // Grab archours from raw data
+        float hh = Mathf.Floor((right_ascension % 1000000) / 10000);
+
+        // Convert hh:mm:ss to degrees
+        float radians = ((hh / 24) + (mm / 1440) + (ss / 86400)) * Mathf.PI * 2;
+
+        return radians;
+    }
+
+    float DeclinationToDegrees(float declination)
+    {
+        
+
+        return 0.0f;
     }
 }
