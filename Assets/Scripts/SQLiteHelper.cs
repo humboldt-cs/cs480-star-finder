609631f9c66099f0d3aaa9896d97ada26c2a8903
peterdanieloliver <peter.daniel.oliver@gmail.com>
2020-04-03 22:30:17 -0700

    got an sqlite database of 9110 stars to work with Unity

---
+++ Assets/Scripts/SQLiteHelper.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Data.Common;
+using Mono.Data.Sqlite;
+using System.IO;
+
+namespace SQLite
+{
+    public class SQLiteHelper
+    {
+        // database fields
+        private const string DBNAME = "Bright_Star_Database";
+        private DbConnection dbConnection;
+        private DbCommand dbCommand;
+
+        // Constructor
+        public SQLiteHelper()
+        {
+            // Create / open connection to local bright star database
+            dbConnection = new SqliteConnection("URI=file:" + Application.persistentDataPath + "/" + DBNAME);
+            dbConnection.Open();
+            dbCommand = dbConnection.CreateCommand();
+        }
+
+        // Destructor
+        ~SQLiteHelper()
+        {
+            dbConnection.Close();
+        }
+
+        // Method for create, insert, update, delete sql statements
+        public int ModifyDB(string sql_non_query_statement)
+        {
+            dbCommand.CommandText = sql_non_query_statement;
+            return dbCommand.ExecuteNonQuery();
+        }
+
+        // Method for query sql statements
+        public DbDataReader QueryDB(string sql_query_statement)
+        {
+            dbCommand.CommandText = sql_query_statement;
+            return dbCommand.ExecuteReader();
+        }
+    }
+}
---
+++ Assets/Scripts/StarDBManager.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using SQLite;
+using System.Data.Common;
+using System.Xml;
+using System.IO;
+
+
+public class StarDBManager : MonoBehaviour
+{
+    private SQLiteHelper sqlhelper;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // create SQL helper object (opens connection to database)
+        sqlhelper = new SQLiteHelper();
+        // create star data table in db if not already created
+        sqlhelper.ModifyDB("CREATE TABLE IF NOT EXISTS star_data (id INTEGER PRIMARY KEY, name TEXT, alt_name TEXT, ra REAL, dec REAL, vmag REAL)");
+        // check to see if table is already populated
+        DbDataReader dbReader = sqlhelper.QueryDB("SELECT * FROM star_data");
+        // if the table is empty, read rows from xml file
+        if (!dbReader.HasRows)
+        {
+            dbReader.Close();
+            // open xml file and read rows into database
+            XmlTextReader xmlReader = new XmlTextReader("Assets/Resources/votable.xml");
+            List<string> xmlDataRow;
+            while (xmlReader.Read())
+            {
+                if (xmlReader.NodeType == XmlNodeType.Element)
+                {
+                    if (xmlReader.Name == "TR")
+                    {
+                        xmlDataRow = xmlReadRow(xmlReader);
+                        string insert_row_statement = "INSERT INTO star_data VALUES ("
+                                                      + xmlDataRow[0] + ",'"
+                                                      + xmlDataRow[1] + "','"
+                                                      + xmlDataRow[2] + "',"
+                                                      + xmlDataRow[3] + ","
+                                                      + xmlDataRow[4] + ","
+                                                      + xmlDataRow[5] + ")";
+                        try
+                        {
+                            sqlhelper.ModifyDB(insert_row_statement);
+                        }
+                        catch
+                        {
+                            continue;
+                        }
+                        
+                    }
+                } 
+            }
+        }
+
+        dbReader.Close();
+        // this is just to test that its working
+        DbDataReader dbReader2 = sqlhelper.QueryDB("SELECT * FROM star_data");
+        string id;
+        string name;
+        string alt_name;
+        string ra;
+        string dec;
+        string vmag;
+        while (dbReader2.Read())
+            {
+                id = dbReader2[0].ToString();
+                name = dbReader2[1].ToString();
+                alt_name = dbReader2[2].ToString();
+                ra = dbReader2[3].ToString();
+                dec = dbReader2[4].ToString();
+                vmag = dbReader2[5].ToString();
+            }
+    }
+
+    // healper for reading stardata.xml table rows
+    private List<string> xmlReadRow(XmlTextReader xmlReader)
+    {
+        List<string> data_row = new List<string> { };
+        bool end_row = false;
+        bool element_open = false;
+        while (end_row == false)
+        {
+            xmlReader.Read();
+            switch (xmlReader.NodeType)
+            {
+                case XmlNodeType.Element:
+                    element_open = true;
+                    break;
+                case XmlNodeType.Text:
+                    data_row.Add(xmlReader.Value);
+                    element_open = false;
+                    break;
+                case XmlNodeType.EndElement:
+                    if (element_open)
+                    {
+                        data_row.Add("");
+                    }
+                    else if (xmlReader.Name == "TR")
+                    {
+                        end_row = true;
+                    }
+                    element_open = false;
+                    break;
+            }
+        }
+        return data_row;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
