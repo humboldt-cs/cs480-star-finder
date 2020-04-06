fb2f4a9555985482a55960fbe53215eee19d34a7
j-feigner <jordan.feigner.r@gmail.com>
2020-04-05 23:42:50 -0700

    Added button click listeners for menu navigation
    Can now open/close settings menu

---
+++ Assets/Scripts/ClickHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickHandler : MonoBehaviour
+{
+    public Button menu_button;
+    public GameObject menu_prefab;
+
+    public void toMenu()
+    {
+        // Create menu prefab
+        Instantiate(menu_prefab, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+}
