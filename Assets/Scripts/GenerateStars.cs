171ac0073660f15e8cfaff879ed1ceb81d3cb3e0
j-feigner <jordan.feigner.r@gmail.com>
2020-04-07 14:47:45 -0700

    Changed format on ASCII catalog
    GenerateStars will now be used for parsing ASCII data for easier constellation data

--- Assets/Scripts/GenerateStars.cs
+++ Assets/Scripts/GenerateStars.cs
@@ -7,39 +7,27 @@ public class GenerateStars : MonoBehaviour
     // Star prefab asset
     public GameObject starPrefab;
 
+    private static string star_data_resource = "catalog";
+    private const int catalog_entry_size = 197;
+
     // Start is called before the first frame update
-    void Start() {
-        // Grab CSV data from text file
-        TextAsset star_data = Resources.Load<TextAsset>("bright_stars");
-        // Split text data by newline
-        string[] stars = star_data.text.Split('\n');
+    void Start()
+    {
+        // Grab file resource
+        TextAsset star_data = Resources.Load<TextAsset>(star_data_resource);
+        // Get bytes
+        byte[] stars = star_data.bytes;
 
         // Loop through star data and instantiate a prefab for each after coordinate conversion
-        for(int i = 1; i < stars.Length - 1; i++) {
-            // Split current star data into comma deliminated substrings
-            string[] current_star = stars[i].Split(',');
+        for(int i = 0; i < stars.Length; i += catalog_entry_size) 
+        {
 
-            float right_ascension;
-            float declination;
-            float apparent_magnitude;
-
-            // TODO: surround these with try/catch to handle exceptions if strings cannot be parsec
-            float.TryParse(current_star[1], out right_ascension);
-            float.TryParse(current_star[2], out declination);
-            float.TryParse(current_star[3], out apparent_magnitude);
-
-            // Unit conversion
-            right_ascension = RightAscensionToRadians(right_ascension);
-            declination = DeclinationToRadians(declination);
-
-            // Instantiate star with name
-            GameObject star = Instantiate(starPrefab, CoordConversion(right_ascension, declination, apparent_magnitude), Quaternion.identity);
-            star.name = current_star[0];
         }
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update() 
+    {
         // Intentionally empty: stars will be fixed
     }
 
@@ -59,48 +47,20 @@ public class GenerateStars : MonoBehaviour
     }
 
     // Conversion from raw RA value to angle in radians for use in sin / cos functions
-    float RightAscensionToRadians(float ra_raw) {
-        float ra_corrected = Mathf.Abs(ra_raw);
-
-        // Grab seconds from raw data and round to nearest second
-        float ss = Mathf.Round(ra_corrected % 100);
-
-        // Grab minutes from raw data
-        float mm = (int)((ra_corrected % 10000) / 100);
-
-        // Grab archours from raw data
-        float hh = (int)((ra_corrected % 1000000) / 10000);
-
-        // Convert hh:mm:ss to radians
-        float radians = ((hh / 24) + (mm / 1440) + (ss / 86400)) * Mathf.PI * 2;
-
-        // Convert to negative value if necessary
-        if(ra_raw < 0)
-        {
-            radians *= -1;
-        }
+    float RightAscensionToRadians(float hr, float min, float sec) {
+        // Convert hr:min:sec to radians
+        float radians = ((hr / 24) + (min / 1440) + (sec / 86400)) * Mathf.PI * 2;
 
         return radians;
     }
 
     // Conversion from raw DEC value to angle in radians for use in sin / cos functions
-    float DeclinationToRadians(float dec_raw) {
-        float dec_corrected = Mathf.Abs(dec_raw);
-
-        // Grab arcseconds from raw data and round to nearest arcsecond
-        float ss = Mathf.Round(dec_corrected % 100);
-
-        // Grab arcminutes from raw data
-        float mm = (int)((dec_corrected % 10000) / 100);
-
-        // Grab degrees from raw data
-        float degrees = (int)((dec_corrected % 1000000) / 10000);
-
+    float DeclinationToRadians(char sign, float deg, float arcmin, float arcsec) {
         // Convert deg:arcminutes:arcseconds to radians
-        float radians = ((degrees / 360) + (mm / 21600) + (ss / 1296000)) * Mathf.PI * 2;
+        float radians = ((deg / 360) + (arcmin / 21600) + (arcsec / 1296000)) * Mathf.PI * 2;
 
         // Convert to negative value if necessary
-        if(dec_raw < 0)
+        if(sign == '-')
         {
             radians *= -1;
         }
