c7453ccfd04b9e032e9642df2bfa532fdc41e074
j-feigner <jordan.feigner.r@gmail.com>
2020-04-14 11:46:47 -0700

    Fixed position errors on viewer objects, added viewer script

---
+++ Assets/Scripts/Viewer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Viewer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Rotate(GPSOrientation.getRotation(0, 90, 0), Space.World);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
