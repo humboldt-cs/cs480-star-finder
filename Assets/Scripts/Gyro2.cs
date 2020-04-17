1813de534e412280367e23f66e060a493724a063
AFitting <asf45@humboldt.edu>
2020-04-16 18:36:11 -0700

    Got started on Gyro2 script.

---
+++ Assets/Scripts/Gyro2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gyro2 : MonoBehaviour
+{
+
+    void Start()
+    {
+        Input.gyro.enabled = true;
+    }
+
+    void Update()
+    {
+        transform.Rotate(-Input.gyro.rotationRateUnbiased.x, -Input.gyro.rotationRateUnbiased.y, -Input.gyro.rotationRateUnbiased.z);
+    }
+}
