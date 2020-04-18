da82b1b077dfd4b2ceb59bba79ea4391a0fb6e63
AFitting <asf45@humboldt.edu>
2020-04-17 20:26:43 -0700

    Did some experimenting, didn't make much progress.

--- Assets/Scripts/DarkModeHandler.cs
+++ Assets/Scripts/DarkModeHandler.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DarkMode : MonoBehaviour
+public class DarkModeHandler : MonoBehaviour
 {
-    public GameObject menu_prefab = (GameObject)Resources.Load("prefabs/Menu", typeof(GameObject));
+    public GameObject menu_prefab; //= (GameObject)Resources.Load("prefabs/Menu", typeof(GameObject));
     public void toDarkMode()
     {
         // change menu color
-        menu_prefab.GetComponent<Renderer>().material.SetColor("Green", Color.green);
-        
+        //menu_prefab.GetComponent<Renderer>().material.SetColor("Green", Color.green);
+        menu_prefab.GetComponent<SpriteRenderer>().color = Color.green;
     }
 }
--- Assets/Scripts/Gyro2.cs
+++ Assets/Scripts/Gyro2.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Gyro2 : MonoBehaviour
 {
+    private Quaternion initialRotation;
+    private Quaternion gyroInitialRotation;
 
     void Start()
     {
         Input.gyro.enabled = true;
+
+        //initialRotation = transform.rotation;
+        //gyroInitialRotation = Input.gyro.attitude;
     }
 
     void Update()
     {
-        //transform.Rotate(-Input.gyro.rotationRateUnbiased.x, -Input.gyro.rotationRateUnbiased.y, -Input.gyro.rotationRateUnbiased.z);
+        //v1: transform.Rotate(-Input.gyro.rotationRateUnbiased.x, -Input.gyro.rotationRateUnbiased.y, -Input.gyro.rotationRateUnbiased.z);
+        //v2: 
         transform.Rotate(-Input.gyro.rotationRateUnbiased.x, -Input.gyro.rotationRateUnbiased.y, 0);
+        //v3:
+        //Quaternion offsetRotation = Quaternion.Inverse(gyroInitialRotation) * Input.gyro.attitude;
+        //transform.rotation = initialRotation * offsetRotation;
     }
 }
