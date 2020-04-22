60ab475812dee3bbf773c0a8e38844f7ea8b725a
AFitting <asf45@humboldt.edu>
2020-04-22 15:38:53 -0700

    Icon update.

--- Assets/Scripts/DarkModeHandler.cs
+++ Assets/Scripts/DarkModeHandler.cs
@@ -5,19 +5,28 @@ using UnityEngine.UI;
 
 public class DarkModeHandler : MonoBehaviour
 {
-    public GameObject menu_prefab; 
+    public GameObject menu_prefab;
     public Toggle DarkModeToggle;
+    public GameObject off_toggle;
     public Material MenuGrey;
     public Material DarkBlue;
+
+    private void Start()
+    {
+        off_toggle.GetComponent<Image>().enabled = false;
+    }
     public void toDarkMode()
     {
+        
         // change menu color
         if (DarkModeToggle.isOn)
         {
             menu_prefab.GetComponent<Image>().material = DarkBlue;
+            off_toggle.GetComponent<Image>().enabled = false;
         }
         else
         {
+            off_toggle.GetComponent<Image>().enabled = true;
             menu_prefab.GetComponent<Image>().material = MenuGrey;
         }
 
