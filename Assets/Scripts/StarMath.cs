5ff30620802d70e9d9783ab70eb02882f419019c
j-feigner <jordan.feigner.r@gmail.com>
2020-04-28 18:22:59 -0700

    Added functions to calculate orientation of the night sky
    Given user location and current date and time, the scene now semi-accurately shows the orientation of the night sky.
    This required significant contribution from external algorithms, sources are posted in code where necessary.

--- Assets/Scripts/StarMath.cs
+++ Assets/Scripts/StarMath.cs
@@ -41,4 +41,57 @@ public static class StarMath
 
         return radians;
     }
+
+    public static Vector3 getRotation(float latitude, float longitude, System.DateTime current_dt)
+    {
+        const int JULIAN_EPOCH = 2451545;
+
+        // Convert current DateTime to UTC
+        System.DateTime dt = current_dt.ToUniversalTime();
+
+        // Convert current date to Julian Day Number and find timespan from J2000.0 reference Julian Day
+        int current_julian_day = StarMath.getJulianDay(dt);
+        int julian_day_difference = current_julian_day - JULIAN_EPOCH;
+
+        // Add current time of day to rotation value using fractional hour
+        float current_hour = dt.Hour + dt.Minute / 60 + dt.Second / 3600;
+
+        // Calculate given rotation for each day / hour that has passed since J2000.0
+        float day_rotation = julian_day_difference % 360;   // 360 degrees in a day
+        float hour_rotation = current_hour * 15;            // 15 degrees in an hour
+
+        // Apply all rotation values
+        double y_rotation = day_rotation + hour_rotation + longitude;
+
+        Vector3 rotation = new Vector3(latitude - 90.0f, (float)y_rotation, 0);
+
+        return rotation;
+    }
+
+    public static int getJulianDay(System.DateTime dt)
+    {
+        // Algorithm written by Bill Jeffries
+        // Source: https://quasar.as.utexas.edu/BillInfo/JulianDatesG.html
+        // Variable names copied from source, they are not significant
+
+        int year = dt.Year;
+        int month = dt.Month;
+        int day = dt.Day;
+
+        if (dt.Month == 1 || dt.Month == 2)
+        {
+            year -= 1;
+            month += 12;
+        }
+
+        int a = year / 100;
+        int b = a / 4;
+        int c = 2 - a + b;
+        int e = Mathf.FloorToInt(365.25f * (year + 4716));
+        int f = Mathf.FloorToInt(30.6001f * (month + 1));
+
+        int julian_day = Mathf.FloorToInt(c + day + e + f - 1524.5f);
+
+        return julian_day;
+    }
 }
--- Assets/Scripts/Viewer.cs
+++ Assets/Scripts/Viewer.cs
@@ -7,12 +7,26 @@ public class Viewer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.Rotate(GPSOrientation.getRotation(0.0f, 0.0f, 0.0f));
+        float test_latitude = 34.227904f;
+        float test_longitude = -116.859673f;
+
+        transform.Rotate(StarMath.getRotation(test_latitude, test_longitude, System.DateTime.Now));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        /*
+        // Inputs for changing rotation of the earth on z/c inputs
+        // Used for testing coordinate conversions, may be reinstated later
+        if(Input.GetKey("z"))
+        {
+            transform.Rotate(Vector3.up, Space.World);
+        }
+        if (Input.GetKey("c"))
+        {
+            transform.Rotate(Vector3.down, Space.World);
+        }
+        */
     }
 }
